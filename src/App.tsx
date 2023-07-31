6218e035f641489a335b0dae5611550873d0b792
Mhedii <mehedi1234.mh@gmail.com>
2023-07-31 13:47:24 +0600

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { Toaster } from './components/ui/Toaster';
+// import MainLayout from './layouts/MainLayout';
+
+function App() {
+  return (
+    <div>
+      {/* <Toaster />
+      <MainLayout /> */}
+      <h1 className="text-3xl underline">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from '@/assets/images/technet-logo-white.png';
+import { RiFacebookBoxFill, RiInstagramLine } from 'react-icons/ri';
+export default function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return <></>;
+}
